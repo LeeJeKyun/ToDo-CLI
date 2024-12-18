6e09f377e1d60adafb9666251b1b0c16fb921fa0
LeeJeKyun <zxsi2003@naver.com>
2024-12-19 00:07:45 +0900

    flag.Parse로 플래그읽히지않는문제해결

--- todo.go
+++ todo.go
@@ -16,17 +16,25 @@ const (
 
 var file []byte
 var works *[]structs.Work
+var filePath string
 
 func init() {
+	filePath, _ = os.UserHomeDir()
+	filePath += "/todoData.json"
 	file = jsonFileRead()
 	works = jsonFileUnMarshal(file)
 }
 
 func main() {
-	s := os.Args[1]
-	switch s {
+
+	action := flag.String("word", "123", "a String")
+	todoName := flag.String("fork", "X", "a string")
+	flag.Parse()
+
+	fmt.Println(*action)
+	//s := os.Args[len(os.Args)-1]
+	switch *action {
 	case ADD:
-		todoName := flag.String("name", "X", "Todo Name")
 		works = appendWork(works, *todoName)
 	case CLEAR:
 
@@ -41,27 +49,29 @@ func main() {
 		panic(err)
 	}
 
-	savefile, err := os.Create("/Users/jekyunlee/json/todoData.json")
+	savefile, err := os.Create(filePath)
 	if err != nil {
 		panic(err)
 	}
 	defer savefile.Close()
-	savefile.Write(marshal)
+	if savefile != nil {
+		savefile.Write(marshal)
+	}
 
 }
 
 func jsonFileRead() []byte {
-	readFile, err := os.ReadFile("/Users/jekyunlee/json/todoData.json")
+	readFile, err := os.ReadFile(filePath)
 	if err != nil {
-		os.Create("/Users/jekyunlee/json/todoData.json")
+		os.Create(filePath)
 	}
 	return readFile
 }
 
 func jsonFileUnMarshal(file []byte) *[]structs.Work {
-	works := new([]structs.Work)
-	json.Unmarshal(file, &works)
-	return works
+	works := make([]structs.Work, 10)
+	json.Unmarshal(file, works)
+	return &works
 }
 
 func appendWork(works *[]structs.Work, name string) *[]structs.Work {
